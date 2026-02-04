039070b9cdb7c140e28210820ae940701c1f0be5
Green Hacker <hhirawat5@gmail.com>
2026-02-04 19:02:39 +0530

    Implement mock LLM function and set up StateGraph with initial nodes and edges

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
-# from langgraph.graph import  StateGraph, MessagesState, START, END
+from langgraph.graph import StateGraph, MessagesState, START, END
 
-import langgraph
-import langchain
+def mock_llm(state: MessagesState):
+    return {"messages": [{"role": "ai", "content": "hello world"}]}
 
+graph = StateGraph(MessagesState)
+graph.add_node(mock_llm)
+graph.add_edge(START, "mock_llm")
+graph.add_edge("mock_llm", END)
+graph = graph.compile()
 
-print(help(langgraph))
-# print(help(langgraph.func))
-# print(help(langgraph.graph))
+graph.invoke({"messages": [{"role": "user", "content": "hi!"}]})
